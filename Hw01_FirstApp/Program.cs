291359aa9f94f5afb2bbc0f4da84d6ad66a0012c
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-02 17:11:42 +0300

    Add project files.

---
+++ Hw01_FirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hw01_FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Isminizi Giriniz : ");
+            string name = Console.ReadLine().Trim();
+            Console.Write("Soyadinizi Giriniz : ");
+            string surname = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Merhaba {name} {surname}");
+
+        }
+    }
+}
