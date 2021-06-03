0a12addc5f46883eaf3e2db12616708b2c041218
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-03 16:02:46 +0300

    Add DecisionStructure if else

---
+++ Hw06_DecisionStructure_IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Hw06_DecisionStructure_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = DateTime.Now.Hour;
+            if(hour>=8 && hour<=11)
+                Console.WriteLine("Gunaydin");
+            else if (hour <= 18)
+                Console.WriteLine("Iyi gunler");
+            else
+                Console.WriteLine("Iyi aksamlar");
+
+            //Ternary If kullanimi
+            string result = hour >= 8 && hour <= 11 ? "Gunaydin" : "Iyi aksamlar";
+            Console.WriteLine(result);
+        }
+    }
+}
