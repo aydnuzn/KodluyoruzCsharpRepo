7cb9e4038539f18456b5131d53e174eb2ed4d87a
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-04 11:05:54 +0300

    Add Error Handling  example2

---
+++ Hw08_DecisionStructure2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hw08_DecisionStructure2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"The Number entered: {number}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message.ToString()}");
+            }
+            finally
+            {
+                Console.WriteLine("Process completed");
+            }
+
+            try
+            {
+                int number1 = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Enter blank value");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("The data type is not suitable");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("The value range is out of bounds");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("Process Completed");
+            }
+        }
+    }
+}
