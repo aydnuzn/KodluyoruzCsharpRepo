618b1501fb6fe50d75b252b080b7286cfe92959e
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-04 12:40:58 +0300

    Add Loops

---
+++ Hw09_Loops/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw09_Loops
+{
+    class Customer
+    {
+        private string _IdentityNumber;
+
+        public string IdentityNumber
+        {
+            get { return _IdentityNumber; }
+            set
+            {
+                if (value.Length == 11 && IdentityControl(value))
+                    this._IdentityNumber = value;
+                else if (value.Equals("0"))
+                    this._IdentityNumber = value;
+                else
+                    Console.WriteLine("Enter only numbers and 11 digits");
+            }
+        }
+
+        private bool IdentityControl(string tcNo)
+        {
+            foreach (char number in tcNo)
+                if (!Char.IsNumber(number))
+                    return false;
+            return true;
+        }
+    }
+}
---
+++ Hw09_Loops/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hw09_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            do
+            {
+                Console.Write("TC Kimlik No giriniz(Exit to '0'): ");
+                customer.IdentityNumber = Console.ReadLine();
+                if (customer.IdentityNumber != null && customer.IdentityNumber.Equals("0"))
+                    break;
+                
+
+                Console.WriteLine("");
+
+            } while (customer.IdentityNumber == null);
+
+            // square drawing
+            Console.Write("Enter Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int nmbr in numbers)
+                Console.Write(nmbr + " ");
+
+            Console.WriteLine();
+
+            //a' dan z' dahil yazdirma
+            char character = 'a';
+            while (character <= 'z')
+                Console.Write(character++);
+
+        }
+    }
+}
