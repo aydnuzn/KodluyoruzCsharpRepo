0c81334076bd73cb2c671da8ab8b3e0eab7aeaf1
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-07 12:59:22 +0300

    Add Second Homework Part3

---
+++ Homework2_Collections3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework2_Collections3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve 
+                       dizinin elemanlarını sıralayan programı yazınız.
+             */
+
+            List<char> vowelList = new List<char>() { 'a', 'e', 'i', 'o', 'u'};
+            List<char> chList = new List<char>();
+
+            Console.Write("Write sentence : ");
+            string sentence = Console.ReadLine();
+            foreach (char ch in sentence)
+                if (vowelList.Contains(ch))
+                    if(!chList.Contains(ch))
+                        chList.Add(ch);
+            
+            chList.Sort();
+            foreach (var item in chList)
+                Console.Write(item + " ");
+        }
+    }
+}
