4f40988fb2e83e7d9e63234a8fcf4b5371cf2364
aydnuzn <60486782+aydnuzn@users.noreply.github.com>
2021-06-07 13:01:43 +0300

    Add Class

---
+++ Hw20_Class/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Hw20_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Access Modifiers
+             * Public
+             * Private
+             * Internal
+             * Protected
+            */
+
+            Employee employee = new Employee()
+            {
+                Name = "Aydin",
+                Surname = "Uzun",
+                No = 31523563,
+                Department = "IT"
+            };
+            Employee employee2 = new Employee()
+            {
+                Name = "Hasan",
+                Surname = "Soran",
+                No = 36734254,
+                Department = "Human Resources"
+            };
+
+            employee.EmployeeInfo();
+            Console.WriteLine("**********************");
+            employee2.EmployeeInfo();
+
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int No { get; set; }
+        public string Department { get; set; }
+
+        public void EmployeeInfo()
+        {
+            Console.WriteLine($"Employee Name: {Name}");
+            Console.WriteLine($"Employee Surname: {Surname}");
+            Console.WriteLine($"Employee No: {No}");
+            Console.WriteLine($"Employee Department: {Department}");
+        }
+
+    }
+}
